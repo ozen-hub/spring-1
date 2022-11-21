069892987edfccb58f65c84de2b232f322ebf869
ozen-hub <info.ozenhub@gmail.com>
2022-11-21 23:04:53 +0530

    4

--- src/main/java/com/bootcamp/controller/CustomerController.java
+++ src/main/java/com/bootcamp/controller/CustomerController.java
@@ -1,8 +1,17 @@
 package com.bootcamp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("api/v1/customer")
 public class CustomerController {
+//=====>http://localhost:8080/api/v1/customer ==> get
+
+    @GetMapping
+    public String init(){
+        return "Hello!, this is my first Spring app";
+    }
 
 }
